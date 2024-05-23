04a910cd44435a52310e6f0c5f7af97c9fa279b1
Abder0514 <abderdrx@gmail.com>
2024-05-23 16:10:49 +0200

    modif test & main

--- app/main.py
+++ app/main.py
@@ -1,17 +1,6 @@
-from fastapi import FastAPI
-import json, os
-from pydantic import BaseModel
-from typing import List, Optional, Dict, Any
-
 from fastapi.testclient import TestClient
 
-# Initialize FastAPI app
-app = FastAPI()
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
+from .main import app
 
 client = TestClient(app)
 
@@ -20,4 +9,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
-
